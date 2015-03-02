a0fc5cdcb9c65ccc2e14e1e56d24147793052cb7
Pavel Veretennikov <vermut@kid.lv>
2015-03-02 13:08:14 +0200

    renaming dev's in udev with filtering

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['udev']['net'] = {}
-default['udev']['banned_modules'] = {}
+default['udev']['banned_modules'] = []
+default['udev']['banned_macs'] = []
---
+++ recipes/rename_interfaces.rb
@@ -0,0 +1,15 @@
+#
+# Forces common ethXX names for exotic interfaces
+#
+# Ignores specific MAC prefixes, like 00:25:90
+
+eth_num = 0
+node['network']['interfaces'].each do |int,data|
+	data['addresses'].each do |addr,addr_info|
+		if addr_info['family'] == 'lladdr' and ! node['udev']['banned_macs'].any? { |mac| addr.start_with? (mac) }
+			Chef::Log.debug("Found valid MAC #{addr} saving as eth#{eth_num}")
+			node.default['udev']['net']["eth#{eth_num}"] = addr
+			eth_num = eth_num+1
+		end
+	end
+end
