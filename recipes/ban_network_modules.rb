c2644b040cdf3689754b38320f648fa45b436055
Pavel Veretennikov <vermut@kid.lv>
2015-03-02 13:07:52 +0200

    try rmmod too

--- recipes/ban_network_modules.rb
+++ recipes/ban_network_modules.rb
@@ -1,8 +1,15 @@
 # 
-# Bans specific network modules
+# Bans specific kernel modules
 #
-node[:udev][:banned_modules].each do |module|
-	file "/etc/modprobe.d/#{module}"
-		content "blacklist #{module}"
+
+node['udev']['banned_modules'].each do |mdl|
+	file "/etc/modprobe.d/blacklist-#{mdl}.conf" do
+		content "blacklist #{mdl}"
+		notifies :run, "execute[update-initramfs -u]"
 	end
+	execute "rmmod #{mdl}"
+end
+
+execute "update-initramfs -u" do
+	action :nothing
 end
